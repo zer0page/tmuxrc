982c05893e573d4cb52d640538994e5a78ea4465
Jackson Chen <jackson.jl.chen@gmail.com>
2013-10-22 16:13:23 -0700

    Added shell script to install load

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,47 @@
 #!/usr/bin/env bash
-cp ./.tmux.conf ~/
+set ex
+DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+LOAD_DIR=/tmp/tmux_load
+LOAD_BIN=/usr/local/bin/tmux-mem-cpu-load
+
+function check_bin(){
+    if [ -x "$LOAD_BIN" ]; then
+        echo "binary is installed."
+        return 0
+    else
+        return 1
+    fi
+}
+function check_load(){
+    if [ -d "$LOAD_DIR" ]; then
+        echo "load directory already exists"
+        return 0
+    else
+        git clone git@github.com:thewtex/tmux-mem-cpu-load.git "$LOAD_DIR"
+        return $?
+    fi
+}
+function install_load(){
+    echo "installing..."
+    cd "$LOAD_DIR"   
+    cmake . && make && sudo make install
+    return $?
+}
+
+function cleanup(){
+    echo "cleaning up..."
+    if [ -d "$LOAD_DIR" ]; then
+        rm -rf "$LOAD_DIR"
+    fi
+}
+echo "$DIR"
+check_bin || ((check_load && install_load)|| cleanup)
+if check_bin; then
+    cp $DIR/.tmux.conf ~/
+    echo "installation successful"
+else
+    echo "installation unsuccessful"
+fi
+
+
+
